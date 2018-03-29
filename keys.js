9a06095b5b5203042044947ddd65cd1524e76179
p shehan <pshehan@ps-MacBook-Air.att.net>
2018-03-28 23:43:41 -0500

    added spotify and twitter

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.jqeybHapLXPhchKVfSrMf5K5q,
-  consumer_secret: process.env.qhaF8DZQbL05gMWUHJsiQZf3ZhMnGnFxpdXK9uogVpRM0DcZv5,
-  access_token_key: process.env.978300358572244992-TiI84HyXZaf3T33rcmfVn8gsCSScmdn,
-  access_token_secret: process.env.kwPcCJB8V9wbeTEnEJPfLTnntn9YVCRQ4yjX5sDlxaXw7
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 
 };
 
 exports.spotify = {
-  id: process.env.S48d397ac09a74942811003efc299f4ca,
-  secret: process.env.f4fbb8101aef41499a6ac2cb3ed3ac0d
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,26 +1,74 @@
 require("dotenv").config();
 
+const Spotify = require("spotify")
+const request = require("request");
+const Twitter = require('twitter');
+
+let functionName = process.argv[2];
+let functionArgument = process.argv[3];
 
 
-var request = require("request");
+function omdb(movieName) {
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
 
-var movieName = process.argv[2];
+    
+    
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy";
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomato Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
 
-// Then create a request to the queryUrl
-// ...
-request(queryUrl, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+function spotifyThis(artistName) {
+    console.log("Artist Name: " + artistName);
+    var queryUrl = "https://api.spotify.com/v1/search?q=" + artistName + '&type=artist&limit=1';
 
-        console.log("Title: " + JSON.parse(body).Title);
-        console.log("Release year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomato Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country: " + JSON.parse(body).Country);
-        console.log("Language: " + JSON.parse(body).Language);
-        console.log("Plot: " + JSON.parse(body).Plot);
-        console.log("Actors: " + JSON.parse(body).Actors);
+    request(queryUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+        }
+
+
+
+    });
+}
+
+function myTweets(twit) {
+    var twit = "search/tweets"  {q: 'pshehan'}, function(error, tweets, response) {
+        console.log(twit);
+
+        });
+
+
+
+    };
+
+
+
+
+function runFunct() {
+    switch (functionName) {
+        case "movie-this":
+            omdb(functionArgument);
+            break;
+        case "spotify-this":
+            spotifyThis(functionArgument);
+            break;
+        case "my-tweets":
+        myTweets(functionArgument);
+
+            break;
     }
-});
+}
+
+runFunct();
